a456199e7a0d6574f2db2c362c797dd1738686ec
Jaemin Lim <jmeeen@gmail.com>
2021-10-10 08:53:34 +0900

    폴더 구조

--- 1-vanilla/js/models/HistoryModel.js
+++ 1-vanilla/js/models/HistoryModel.js
@@ -1,26 +1,30 @@
 export default {
   data: [
-    { keyword: '검색기록2', date: '12.03' },
-    { keyword: '검색기록1', date: '12.02'},
-    { keyword: '검색기록0', date: '12.01' },
+    { keyword: "검색기록2", date: "12.03" },
+    { keyword: "검색기록1", date: "12.02" },
+    { keyword: "검색기록0", date: "12.01" },
   ],
 
+  // 그냥 데이터를 리턴한다. promise를 이용한 이유는 서버에서 비동기로 데이터를 가져와야하는 경우가 있기 때문.
   list() {
-    return Promise.resolve(this.data)
+    return Promise.resolve(this.data);
   },
-  
-  add(keyword = '') {
-    keyword = keyword.trim()
-    if (!keyword) return 
-    if (this.data.some(item => item.keyword === keyword)) {
-      this.remove(keyword)
+
+  // 추가될 검색어를 받아, 데이터의 유무를 체크하고, 날짜를 추가하여 등록
+
+  add(keyword = "") {
+    keyword = keyword.trim();
+    if (!keyword) return;
+    if (this.data.some((item) => item.keyword === keyword)) {
+      this.remove(keyword);
     }
 
-    const date = '12.31'
-    this.data = [{keyword, date}, ...this.data]
+    const date = "12.31";
+    this.data = [{ keyword, date }, ...this.data];
   },
-  
+
+  // 키워드를 받아서 그 키워드인 것을 삭제
   remove(keyword) {
-    this.data = this.data.filter(item => item.keyword !== keyword)
-  }
-}
+    this.data = this.data.filter((item) => item.keyword !== keyword);
+  },
+};
--- 1-vanilla/js/views/View.js
+++ 1-vanilla/js/views/View.js
@@ -1,30 +1,32 @@
-const tag = '[View]'
+const tag = "[View]";
 
 export default {
   init(el) {
-    if (!el) throw el
-    this.el = el
-    return this
+    if (!el) throw el;
+    this.el = el;
+    return this;
   },
 
+  // 이벤트와 핸들러를 인자로 받아서 엘리먼트에서 이벤트가 발생했을때 핸들러 실행
   on(event, handler) {
-    this.el.addEventListener(event, handler)
-    return this
+    this.el.addEventListener(event, handler);
+    return this;
   },
 
+  // 스스로 이벤트를 방출함. 커스텀이벤트는 이름과 디테일 데이터 객체를 받아서 엘리먼트에 이벤트를 방출
   emit(event, data) {
-    const evt = new CustomEvent(event, { detail: data })
-    this.el.dispatchEvent(evt)
-    return this
+    const evt = new CustomEvent(event, { detail: data });
+    this.el.dispatchEvent(evt);
+    return this;
   },
 
   hide() {
-    this.el.style.display = 'none'
-    return this
+    this.el.style.display = "none";
+    return this;
   },
 
   show() {
-    this.el.style.display = ''
-    return this
-  }
-}
+    this.el.style.display = "";
+    return this;
+  },
+};

7a0793827feb0b2bcfa4e173dd67c81ce8d7c9f6
Jaemin Lim <jmeeen@gmail.com>
2021-10-16 06:57:33 +0900

    탭 구현

--- 1-vanilla/js/controller/MainController.js
+++ 1-vanilla/js/controller/MainController.js
@@ -1,6 +1,7 @@
 import FormView from "../views/FormView.js";
 import ResultView from "../views/ResultView.js";
 import SearchModel from "../models/SearchModel.js";
+import TabView from "../views/tabView.js";
 
 const tag = "[MainController]";
 
@@ -10,7 +11,12 @@ export default {
     FormView.setup(document.querySelector("form"))
       .on("@submit", (e) => this.onSubmit(e.detail.input))
       .on("@reset", (e) => this.onResetForm());
+
+    TabView.setup(document.querySelector("#tabs"));
     ResultView.setup(document.querySelector("#search-result"));
+
+    this.selectedTab = "추천 검색어";
+    this.renderView();
   },
 
   search(query) {
@@ -18,6 +24,12 @@ export default {
     SearchModel.list(query).then((data) => {
       this.onsearchResult(data);
     });
+  },
+
+  renderView() {
+    console.log(tag, "renderView");
+    TabView.setActionTab(this.selectedTab);
+    ResultView.hide();
   },
   onSubmit(input) {
     console.log(tag, "onSubmit()", input);
